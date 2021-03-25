ea04d5e44cdd7175a5e4313f51499548bfdc74a9
ptspillane <ptspillane@wpi.edu>
2021-03-25 12:34:55 -0400

    added name

--- src/main/java/edu/wpi/teamname/views/FXMLLoaderProvider.java
+++ src/main/java/edu/wpi/teamname/views/FXMLLoaderProvider.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname.views;
 //import com.google.inject.AbstractModule;
 //import com.google.inject.Injector;
 //import com.google.inject.Provides;
-import javafx.fxml.FXMLLoader;
+//import javafx.fxml.FXMLLoader;
 
 /*public class FXMLLoaderProvider extends AbstractModule {
 
